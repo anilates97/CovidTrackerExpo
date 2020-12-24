1fe9cd531a73b2d07117d921ff29e5e836ec8396
Anıl Ateş <anilates.97@gmail.com>
2020-12-24 12:01:25 +0100

    addappjs

---
+++ App.js
@@ -0,0 +1,62 @@
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SplashScreen } from 'expo';
+import * as Font from 'expo-font';
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LinkingConfiguration from './navigation/LinkingConfiguration';
+
+const Stack = createStackNavigator();
+
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+
+  // Uygulamayı oluşturmadan önce ihtiyacımız olan tüm kaynakları veya verileri yüklüyoruz
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load fonts
+        await Font.loadAsync({
+          ...Ionicons.font,
+        });
+
+        
+      } catch (e) {
+        // Aynı şekilde bu hata bilgilerini bir hata raporlama hizmetine vermek isteyebiliriz
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return null;
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+        <NavigationContainer linking={LinkingConfiguration}>
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
